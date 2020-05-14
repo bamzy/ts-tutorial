2e5efec7ee6c670dfbaeef10233a6bb7fa8d69ee
Bamdad <aghilide@ualberta.ca>
2020-05-14 16:07:53 -0400

    WIP

---
+++ app.js
@@ -0,0 +1,1 @@
+console.log("Tour code goes here");
--- using-ts.ts
+++ using-ts.ts
@@ -2,14 +2,12 @@ const button = document.querySelector("button");
 const input1 = document.getElementById("firstNumber") as HTMLInputElement;
 const input2 = document.getElementById("secondNumber") as HTMLInputElement;
 
-
-function betterAdd(a: number,b: number){
-    if (typeof a === "number" && typeof b === "number"){
-        return a+b;
-    } else
-        return +a + +b;
+function betterAdd(a: number, b: number) {
+  if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  } else return +a + +b;
 }
 
-button.addEventListener("click", function(){
-    console.log(betterAdd(+input1.value,+input2.value));
+button.addEventListener("click", function () {
+  console.log(betterAdd(+input1.value, +input2.value));
 });
