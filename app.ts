56277f3ac2dc6e846962de058a2c601f6460f4fd
Bamdad <aghilide@ualberta.ca>
2020-05-15 12:35:40 -0400

    This is how you define object types

--- app.ts
+++ app.ts
@@ -1,5 +1,10 @@
 console.log("Tour code goes here");
 let a: number = 5;
-a = "b";
-a = true;
 let b: number = 6;
+
+const person: {name: string,age: number}  = {
+  name: "Maximilan",
+  age: 10,
+};
+
+console.log(person.age);
