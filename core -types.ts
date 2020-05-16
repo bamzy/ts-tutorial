418c7fe06365f41b2d223409ee03b8a65db13b6e
Bamdad <aghilide@ualberta.ca>
2020-05-16 12:32:41 -0400

    you can exclue stuff in tsconfig.json

--- core -types.ts
+++
@@ -1,9 +0,0 @@
-// and example of type alias plus union
-type Combinable = number | string | boolean | 'A';
-function combine(input1: number, input2: number, showFlag: Combinable): string {
-  return input1 + input2;
-} 
-
-
-
-console.log(combine(1, 2, ));
